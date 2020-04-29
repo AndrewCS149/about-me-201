df090924cca8d2c5de11b45f0e17f70f7c090546
AndrewCS149 <Andrewcs149@gmail.com>
2020-04-28 19:13:10 -0700

    Comment out console.logs()

--- js/app.js
+++ js/app.js
@@ -9,50 +9,50 @@ var dogPerson = prompt('Am I a dog person?').toLowerCase();
 
 if(dogPerson === 'yes' || dogPerson === 'y') {
   alert('You are correct!');
-  console.log('User answered correctly');
+  // console.log('User answered correctly');
 } else {
   alert('WRONG');
-  console.log('User answered incorrectly');
+  // console.log('User answered incorrectly');
 }
 
 // Prompt 2
 var wearGlasses = prompt('Do I wear glasses?').toLowerCase();
 if(wearGlasses === 'yes' || wearGlasses === 'y') {
   alert('You are correct!');
-  console.log('User answered correctly');
+  // console.log('User answered correctly');
 } else {
   alert('WRONG');
-  console.log('User answered incorrectly');
+  // console.log('User answered incorrectly');
 }
 
 // Prompt 3
 var vegan = prompt('Am I vegan?').toLowerCase();
 if(vegan === 'yes' || vegan === 'y') {
   alert('WRONG');
-  console.log('User answered incorrectly');
+  // console.log('User answered incorrectly');
 } else {
   alert('You are correct!');
-  console.log('User answered correctly');
+  // console.log('User answered correctly');
 }
 
 // Prompt 4
 var officeFan = prompt('Do you think that I am a fan of the office?').toLowerCase();
 if(officeFan === 'yes' || officeFan === 'y') {
   alert('You are correct!');
-  console.log('User answered correctly');
+  // console.log('User answered correctly');
 } else {
   alert('WRONG');
-  console.log('User answered incorrectly');
+  // console.log('User answered incorrectly');
 }
 
 // Prompt 5
 var height = prompt('Am I above six feet tall?').toLowerCase();
 if(height === 'yes' || height === 'y') {
   alert('WRONG');
-  console.log('User answered incorrectly');
+  // console.log('User answered incorrectly');
 } else {
   alert('You are correct!');
-  console.log('User answered correctly');
+  // console.log('User answered correctly');
 }
 
 // Ask user about themselves
