b7a9ce67e59dd4d00d0bedcda8f34c1ef7166c56
AndrewCS149 <Andrewcs149@gmail.com>
2020-04-28 19:10:58 -0700

    Create final greeting alert in app.js.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 'use strict';
 
+/* The following five prompts asks user yes or no
+questions about myself and alerts the user if they
+are correct or not. */
+
 // Prompt 1
 var dogPerson = prompt('Am I a dog person?').toLowerCase();
 
@@ -50,3 +54,11 @@ if(height === 'yes' || height === 'y') {
   alert('You are correct!');
   console.log('User answered correctly');
 }
+
+// Ask user about themselves
+var userName = prompt('Now that you\'ve got to know me just a bit, it\'s time to tell me a little about yourself. What is your name?');
+
+var homeTown = prompt('Well hello ' + userName + '! What city or town are you from?');
+
+alert('I see, ' + homeTown + ' is a little bit of a drive from my hometown of Rockford, Illinois. Well anyways it was nice to meet you ' + userName + '. I hope you have a wonderful day and enjoy reading a little more about me on this webpage. Good day!');
+
