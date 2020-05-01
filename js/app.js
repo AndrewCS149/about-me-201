16699187aa6f2a3d341237e7a185cf4e3551b602
AndrewCS149 <Andrewcs149@gmail.com>
2020-04-30 19:32:57 -0700

    Create one function for all y/n questions

--- js/app.js
+++ js/app.js
@@ -8,119 +8,69 @@ are correct or not. */
 // questions were answered
 var score = 0;
 
-// Prompt 1
-// console.log('Ask user if I am a dog person.');
+// 2d array of questions and answers
+var yesNoQuestions = [
+  ['Am I a dog person?', 'yes', 'no'],
+  ['Do I wear glasses?', 'yes', 'no'],
+  ['Am I vegan?', 'no', 'yes'],
+  ['Do you think that I am a fan of the office?', 'yes', 'no'],
+  ['Am I above six feet tall?', 'no', 'yes']
+];
 
-function dog() {
-  
-  var dogPerson = prompt('Am I a dog person?').toLowerCase();
-  if(dogPerson === 'yes' || dogPerson === 'y') {
-    // console.log('User answered correctly');
-    alert('You are correct!');
-    score++;
-  } else {
-    alert('WRONG');
-    // console.log('User answered incorrectly');
+// Wrong answer responses that are given to the user when the
+// incorrect answer is inputted
+var wrongAnswers = [
+  'Wrong! I like dogs more than most people.',
+  'Nope! I have trouble reading the Chick-fil-A menu.',
+  'Wrong! Nothing is better than a medium rare steak.',
+  'How dare you. I\'m not superstitious, but I am a little stitious.',
+  'Incorrect. I am 70 inches tall'
+];
+
+
+function questions() {
+
+  for(var i = 0; i < yesNoQuestions.length; i++) {
+    var userResponse = prompt(yesNoQuestions[i][0]).toLowerCase();
+
+    if(userResponse === yesNoQuestions[i][1] ||
+      userResponse === yesNoQuestions[i][1][0]) {
+      alert('You are correct!');
+      score++;
+      console.log('User answered correctly');
+    } else if(userResponse === yesNoQuestions[i][2] ||
+              userResponse === yesNoQuestions[i][2][0]) {
+      alert(wrongAnswers[i]);
+      score++;
+      console.log('User answered incorrectly');
+    }
   }
 }
-
-dog();
-
-
-// Prompt 2
-// console.log('Ask user if I wear glasses.');
-function glasses(){
-  var wearGlasses = prompt('Do I wear glasses?').toLowerCase();
-  if(wearGlasses === 'yes' || wearGlasses === 'y') {
-    // console.log('User answered correctly');
-    alert('You are correct!');
-    score++;
-  } else {
-    // console.log('User answered incorrectly');
-    alert('WRONG');
-  }
-}
-glasses();
-
-
-// // Prompt 3
-// console.log('Ask user if I am vegan.');
-
-function vegan(){
-  var vegan = prompt('Am I vegan?').toLowerCase();
-  if(vegan === 'yes' || vegan === 'y') {
-    // console.log('User answered incorrectly');
-    alert('WRONG');
-  } else {
-    // console.log('User answered correctly');
-    alert('You are correct!');
-    score++;
-  }
-}
-
-vegan();
-
-// // Prompt 4
-// console.log('Ask user if I am a fan of the office.');
-
-function theOffice(){
-  var officeFan = prompt('Do you think that I am a fan of the office?').toLowerCase();
-  if(officeFan === 'yes' || officeFan === 'y') {
-    // console.log('User answered correctly');
-    alert('You are correct!');
-    score++;
-  } else {
-    alert('WRONG');
-    // console.log('User answered incorrectly');
-  }
-}
-
-theOffice();
-
-// Prompt 5
-// console.log('Ask user if I am above 6 feet tall.');
-
-function height(){
-  var height = prompt('Am I above six feet tall?').toLowerCase();
-  if(height === 'yes' || height === 'y') {
-    alert('WRONG');
-    // console.log('User answered incorrectly');
-  } else {
-    // console.log('User answered correctly');
-    alert('You are correct!');
-    score++;
-  }
-}
-
-height();
-
-
 
 // Prompt 6
 // Make user guess how many MCU films there are in total
-
 function filmTotal(){
   var number = Math.floor(Math.random() * 100);
   var numGuess = prompt('Can you guess the secret number between 1 and 100?');
   var i = 0;
   // console.log('Ask user how many MCU films there are.');
-  
+
   while(i < 4) {
     i++;
-  
+
     if(numGuess == number) {
       // console.log('User answered correctly');
       alert('You are correct!');
       score++;
       break;
     }
-  
+
     if(i === 4) {
       // console.log('User answered incorrectly');
       alert('You lost... The correct answer is ' + number);
       break;
     }
-  
+
     if(numGuess < number) {
       numGuess = prompt('Too low. Try Again: ');
     } else {
@@ -129,22 +79,18 @@ function filmTotal(){
   }
 }
 
-filmTotal();
-
-
 // Prompt 7
 // Make user guess which MCU characters are my favorite.
 // console.log('Ask user who my favorite MCU character is.');
-
 function favoriteCharacter(){
   var favCharacter = ['iron man', 'thor', 'captain america', 'loki'];
   var answer;
   var characterCount = 0;
-  
+
   while(!answer) {
-  
+
     var characterGuess = prompt('Who is one of my favorite MCU characters? ').toLowerCase();
-  
+
     if(characterGuess === favCharacter[0] ||
         characterGuess === favCharacter[1] ||
         characterGuess === favCharacter[2] ||
@@ -157,7 +103,7 @@ function favoriteCharacter(){
       alert('WRONG');
       characterCount++;
     }
-  
+
     if(characterCount === 6) {
       // console.log('User answered incorrectly');
       alert('Unfortunately, you have lost. My favorite characters are Iron man, Thor, Captain America and Loki.');
@@ -166,30 +112,24 @@ function favoriteCharacter(){
   }
 }
 
-favoriteCharacter();
 
 // alert user how many questions they have answered correctly.
-
-
-function congrats(){
+function showScore(){
   alert('Congratulations! You have answered ' + score + ' out of 7 questions correctly.');
 }
 
-congrats();
-
-
-
-
-
-// // Ask user about themselves
-
+// Ask user about themselves
 function greet(){
   var userName = prompt('Now that you\'ve got to know me just a bit, it\'s time to tell me a little about yourself. What is your name?');
-  
+
   var homeTown = prompt('Well hello ' + userName + '! What city or town are you from?');
-  
+
   alert('I see, ' + homeTown + ' is a little bit of a drive from my hometown of Rockford, Illinois. Well anyways it was nice to meet you ' + userName + '. I hope you have a wonderful day and enjoy reading a little more about me on this webpage. Good day!');
 }
 
+questions();
+showScore();
+filmTotal();
+favoriteCharacter();
 greet();
 
